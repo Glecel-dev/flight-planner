b85a77395ecbf308f667817ec58206592ddd2e66
Gledi Çela <gledi11011cela@gmail.com>
2022-12-10 13:46:19 +0100

    APP-0000: Flight Planner:
    Changes Made:
    1. Added Remove Flight Logic and DOM component

--- src/modules/map/components/flight-list/flight-list.component.ts
+++ src/modules/map/components/flight-list/flight-list.component.ts
@@ -1,17 +1,20 @@
-import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+import {Component, EventEmitter, Output} from '@angular/core';
 import {Observable} from "rxjs";
 import {Flight} from "../../../../common/interfaces/flight.interface";
 import {Store} from "@ngrx/store";
 import {selectAllFlights} from "../../../../common/store/flight/flight.selector";
+import {removeFlight} from "../../../../common/store/flight/flight.action";
 
 @Component({
   selector: 'app-flight-list',
   templateUrl: './flight-list.component.html',
   styleUrls: ['./flight-list.component.scss']
 })
-export class FlightListComponent implements OnInit{
+export class FlightListComponent {
+
   @Output()
   editFlightCoordinates = new EventEmitter();
+
   // readonly flights$ = this.flightService.flights$;
   readonly flights$ :Observable<Flight[]>
   constructor(
@@ -24,7 +27,8 @@ export class FlightListComponent implements OnInit{
     this.flights$ = store.select(selectAllFlights)
   }
 
-  ngOnInit() {
+  removeFlight(flight: Flight){
+    this.store.dispatch(removeFlight(flight))
   }
 
 }
